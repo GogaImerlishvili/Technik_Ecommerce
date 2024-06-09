5ee96ab8a65807facc201a7f52cba53d52ccba21
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-06-09 23:38:48 +0400

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../App";
+import { createBrowserRouter} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default router;
