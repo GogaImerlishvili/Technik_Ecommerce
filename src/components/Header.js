30e882dc8cbae5274851a30f66a8e97f799b6881
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-06-19 22:44:53 +0400

    added header component with search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="h-16 shadow-md bg:white">
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+        <div>
+          <Link><Logo w={90} h={50} /></Link>
+        </div>
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2 ">
+          <input
+            type="text"
+            placeholder="search product here"
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+            <GrSearch />
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="text-3xl cursor-pointer">
+            <FaRegUserCircle />
+          </div>
+          <div className="text-3xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+          <div>
+              <Link to="/login" className="ml-4 px-3 py-1 text-white bg-red-600 rounded-full hover:bg-red-700 w-[100px]">Login</Link>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
